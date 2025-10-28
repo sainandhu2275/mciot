e7db34dfa1f42e162a966b677b6debb430dddd60
sainandhu2275 <sainandhu2275@gmail.com>
2025-10-28 22:13:35 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -4,11 +4,10 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import App from './App.jsx'
 import './index.css'
 
-// Redirect component to handle external URLs
+// Custom redirect component
 function RedirectToExternal({ url }) {
   React.useEffect(() => {
-    // Redirect to the external site in the same tab
-    window.location.replace(url)
+    window.location.replace(url)  // open in same tab
   }, [url])
 
   return <p>Redirecting...</p>
@@ -18,41 +17,13 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
       <Routes>
-        {/* Default home route */}
         <Route path="/" element={<App />} />
-
-        {/* Redirect route */}
-        <Route
-          path="/task6"
-          element={
-            <RedirectToExternal url="https://wokwi.com/projects/383981664359646209" />
-          }
-        />
-        <Route
-          path="/task9"
-          element={
-            <RedirectToExternal url="https://wokwi.com/projects/375833305298579457" />
-          }
-        />
-        <Route
-          path="/task10"
-          element={
-            <RedirectToExternal url="https://wokwi.com/projects/415371687258065921" />
-          }
-        />
-        <Route
-          path="/task11"
-          element={
-            <RedirectToExternal url="https://wokwi.com/projects/409312856465230849" />
-          }
-        />
-        <Route
-          path="/task12"
-          element={
-            <RedirectToExternal url="https://wokwi.com/projects/383260777330575361" />
-          }
-        />
+        <Route path="/task6" element={<RedirectToExternal url="https://wokwi.com/projects/383981664359646209" />} />
+        <Route path="/task9" element={<RedirectToExternal url="https://wokwi.com/projects/375833305298579457" />} />
+        <Route path="/task10" element={<RedirectToExternal url="https://wokwi.com/projects/415371687258065921" />} />
+        <Route path="/task11" element={<RedirectToExternal url="https://wokwi.com/projects/409312856465230849" />} />
+        <Route path="/task12" element={<RedirectToExternal url="https://wokwi.com/projects/383260777330575361" />} />
       </Routes>
     </BrowserRouter>
-  </React.StrictMode>,
+  </React.StrictMode>
 )
