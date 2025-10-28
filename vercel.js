120a5175f1514344c8f33f25b467fa604b87ff8c
sainandhu2275 <sainandhu2275@gmail.com>
2025-10-28 22:15:38 +0530

    Update vercel.js

--- vercel.js
+++ vercel.js
@@ -1,31 +1,4 @@
 {
-  "redirects": [
-    {
-      "source": "/task6",
-      "destination": "https://wokwi.com/projects/383981664359646209",
-      "permanent": false
-    },
-    {
-      "source": "/task9",
-      "destination": "https://wokwi.com/projects/375833305298579457",
-      "permanent": false
-    },
-    {
-      "source": "/task10",
-      "destination": "https://wokwi.com/projects/415371687258065921",
-      "permanent": false
-    },
-    {
-      "source": "/task11",
-      "destination": "https://wokwi.com/projects/409312856465230849",
-      "permanent": false
-    },
-    {
-      "source": "/task12",
-      "destination": "https://wokwi.com/projects/383260777330575361",
-      "permanent": false
-    }
-  ],
   "rewrites": [
     { "source": "/(.*)", "destination": "/" }
   ]
