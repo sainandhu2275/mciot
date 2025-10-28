b7e2d6e0c5decf3ae85b06a0f3e7018f6d782266
Raja Sai Datha Sushanth <rajasaidathasushanth2006@gmail.com>
2025-10-28 22:02:44 +0530

    Updated code and added vercel redirect config

---
+++ vercel.js
@@ -0,0 +1,29 @@
+{
+  "redirects": [
+    {
+      "source": "/task6",
+      "destination": "https://wokwi.com/projects/383981664359646209",
+      "permanent": true
+    },
+    {
+      "source": "/task9",
+      "destination": "https://wokwi.com/projects/375833305298579457",
+      "permanent": true
+    },
+    {
+      "source": "/task10",
+      "destination": "https://wokwi.com/projects/415371687258065921",
+      "permanent": true
+    },
+    {
+      "source": "/task11",
+      "destination": "https://wokwi.com/projects/409312856465230849",
+      "permanent": true
+    },
+    {
+      "source": "/task12",
+      "destination": "https://wokwi.com/projects/383260777330575361",
+      "permanent": true
+    }
+  ]
+}
