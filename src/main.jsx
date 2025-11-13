53cf536573c533dafe0ae5f1e27e561444e4ae37
sainandhu2275 <sainandhu2275@gmail.com>
2025-11-13 23:40:28 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -33,6 +33,10 @@ function Home() {
         <li><Link to="/task4">Task 9</Link></li>
         <li><Link to="/task5">Task 8</Link></li>
         <li><Link to="/task6">Task 6</Link></li>
+        <li><Link to="/task7">Task 5</Link></li>
+        <li><Link to="/task44">Task 4</Link></li>
+        <li><Link to="/task33">Task 3</Link></li>
+        <li><Link to="/task22">Task 2</Link></li>
         <li><Link to="/task1">Task 1</Link></li>
       </ul>
     </div>
@@ -73,7 +77,7 @@ function App() {
         />
         <Route
           path="/task7"
-          element={<RedirectToExternal url="https://wokwi.com/projects/383981664359646209" />}
+          element={<RedirectToExternal url="hhttps://wokwi.com/projects/446485620429689857" />}
         />
         <Route
           path="/task8"
@@ -82,6 +86,18 @@ function App() {
         <Route
           path="/task1"
           element={<RedirectToExternal url="https://www.tinkercad.com/things/kp0smgPOPnP/editel?lessonid=EFU6PEHIXGFUR1J&projectid=OGK4Q7VL20FZRV9&collectionid=OMOZACHJ9IR8LRE&title=Editing%20Components&sharecode=7i3elgVHFVDQxopeKFeOTiwYh-QbDqCqACa1H7oJTx8" />}
+        />
+        <Route
+          path="/task44"
+          element={<RedirectToExternal url="https://www.tinkercad.com/things/0zpgneQANV1-copy-of-copy-of-copy-of-task4/editel?returnTo=https%3A%2F%2Fwww.tinkercad.com%2Fdashboard%2Fdesigns%2Fcircuits&sharecode=NSScWwT6vd8WfJKV97gbDoIy8omDfLEHSwENCWxQhFE" />}
+        />
+        <Route
+          path="/task33"
+          element={<RedirectToExternal url="https://wokwi.com/projects/415371687258065921" />}
+        />
+        <Route
+          path="/task22"
+          element={<RedirectToExternal url="https://wokwi.com/projects/445984021591846913" />}
         />
       </Routes>
     </Router>
